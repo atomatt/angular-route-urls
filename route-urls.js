5451b4d2c389d2e95afc6e364b74b8d0874ee7a4
Igor Okulist <okigan@gmail.com>
2014-04-30 08:28:16 -0700

    Refactor to allow re-use with $location service
    Refactor to allow re-use with $location service when used inside controller code.

--- route-urls.js
+++ route-urls.js
@@ -3,19 +3,25 @@ var m = angular.module("routeUrls", []);
 m.factory("urls", function($route) {
 
     var pathsByName = {};
-    angular.forEach($route.routes, function(route, path) {
+    angular.forEach($route.routes, function (route, path) {
         if (route.name) {
             pathsByName[route.name] = path;
         }
     });
 
+    var path = function (name, params) {
+        var url = pathsByName[name] || "/";
+        angular.forEach(params || {}, function (value, key) {
+            url = url.replace(new RegExp(":" + key + "(?=/|$)"), value);
+        });
+        return url;
+    };
+
     return {
-        href: function(name, params) {
-            var url = pathsByName[name] || "/";
-            angular.forEach(params || {}, function(value, key) {
-                url = url.replace(new RegExp(":" + key + "(?=/|$)"), value);
-            });
-            return "#" + url;
+        path: path,
+
+        href: function (name, params) {
+            return "#" + path(name, params);
         }
     };
 });
