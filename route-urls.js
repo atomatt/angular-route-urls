711c564e7975141c7f9c594e45fc7c9ae72ce12b
Matt Goodall <matt.goodall@gmail.com>
2014-06-13 16:39:56 +0100

    Add a few comments and make whitespace more consistent.
    I realise 2 spaces is common in JS-land but I started using 4 so I win
    for now ;).

--- route-urls.js
+++ route-urls.js
@@ -2,6 +2,7 @@ angular.module("routeUrls", [])
 
 .factory("urls", function($route) {
 
+    // Cache the routing paths for any named routes.
     var pathsByName = {};
     angular.forEach($route.routes, function (route, path) {
         if (route.name) {
@@ -9,15 +10,17 @@ angular.module("routeUrls", [])
         }
     });
 
+    // Param name to replacement regex cache.
     var regexs = {};
 
+    // Build a path for the named route from the route's URL and the given
+    // params.
     var path = function (name, params) {
         var url = pathsByName[name] || "/";
         angular.forEach(params || {}, function (value, key) {
             var regex = regexs[key];
-
             if (regex === undefined) {
-              regex = regexs[key] = new RegExp(":" + key + "(?=/|$)");
+                regex = regexs[key] = new RegExp(":" + key + "(?=/|$)");
             }
             url = url.replace(regex, value);
         });
@@ -26,7 +29,6 @@ angular.module("routeUrls", [])
 
     return {
         path: path,
-
         href: function (name, params) {
             return "#" + path(name, params);
         }
